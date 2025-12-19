ad1c9e7fd0cd3f5331ef3c02cdf35dd0bd52cf92
maiangabriel <gabriel13rj@hotmail.com>
2025-12-18 22:58:42 -0300

    dfgsdgsd

--- ETL/load/supabase_carga.py
+++ ETL/load/supabase_carga.py
@@ -1,22 +1,26 @@
-from psycopg2.extras import execute_batch
+import psycopg2
 
-def load_supabase(df, cur):
+def load_supabase(df, database_url):
+    conn = psycopg2.connect(database_url)
+    cur = conn.cursor()
+
     query = """
-        INSERT INTO tracks_played
-        (track, artist, album, genre, played_at)
-        VALUES (%s, %s, %s, %s, %s)
-        ON CONFLICT DO NOTHING;
+        insert into public.base_musicas
+        (run_date, track, artist, album, genre, played_at)
+        values (%s, %s, %s, %s, %s, %s)
+        on conflict do nothing
     """
-
-    data = [
-        (
-            r["track"],
-            r["artist"],
-            r["album"],
-            r["genre"],
-            r["played_at"],
+    for _, r in df.iterrows():
+        cur.execute(
+            query,
+            (
+                r["track"],
+                r["artist"],
+                r["album"],
+                r["genre"],
+                r["played_at"],
+            )
         )
-        for _, r in df.iterrows()
-    ]
-
-    execute_batch(cur, query, data, page_size=100)
+    conn.commit()
+    cur.close()
+    conn.close()
