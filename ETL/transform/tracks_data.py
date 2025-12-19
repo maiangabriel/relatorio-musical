236b0c991f611395f2d11cb15e87c0868cfb3981
maiangabriel <gabriel13rj@hotmail.com>
2025-12-19 08:56:39 -0300

    ajuste tz2

--- ETL/transform/tracks_data.py
+++ ETL/transform/tracks_data.py
@@ -6,12 +6,12 @@ TIMEZONE = ZoneInfo("America/Sao_Paulo")
 
 def transform_tracks(raw_tracks, genre_func):
     rows = []
-    played_at = datetime.fromtimestamp(
-            int(t["date"]["uts"]),
-            tz=TIMEZONE
-        )
+   
 
     for t in raw_tracks:
+        played_at = datetime.fromtimestamp(
+            int(t["date"]["uts"]),
+            tz=TIMEZONE)
         # Ignora música "now playing" (não tem timestamp)
         if "@attr" in t and t["@attr"].get("nowplaying") == "true":
             continue
